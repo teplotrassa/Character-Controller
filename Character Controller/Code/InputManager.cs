0b7e28eb9b786c3bd348a8970276738d96dacb95
Danya <glushko1337@gmail.com>
2022-01-17 17:13:55 +0500

    Added InputManager class

---
+++ Character Controller/Code/InputManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Character_Controller
+{
+    static class InputManager
+    {
+        static Vector2 GetDirectionalInputVector()
+        {
+            return Vector2.Zero;
+        }
+    }
+}
