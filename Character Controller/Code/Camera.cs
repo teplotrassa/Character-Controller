d89f63b545cfe50b89f93b9f93fcc27b8e830459
Danya <glushko1337@gmail.com>
2022-01-29 22:38:38 +0500

    Added a function to get visible area in world space

--- Character Controller/Code/Camera.cs
+++ Character Controller/Code/Camera.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Character_Controller
@@ -47,6 +42,23 @@ namespace Character_Controller
                 Matrix.CreateRotationZ(_rotation) *
                 Matrix.CreateScale(new Vector3(_zoom, _zoom, 1)) *
                 Matrix.CreateTranslation(new Vector3(graphicsDevice.Viewport.Width * 0.5f, graphicsDevice.Viewport.Height * 0.5f, 0f));
+        }
+
+        public Rectangle GetVisibleArea(GraphicsDevice graphicsDevice)
+        {
+            var _screenSize = new Vector2(graphicsDevice.Viewport.Width, graphicsDevice.Viewport.Height);
+            var inverseViewMatrix = Matrix.Invert(_transform);
+            var tl = Vector2.Transform(Vector2.Zero, inverseViewMatrix);
+            var tr = Vector2.Transform(new Vector2(_screenSize.X, 0), inverseViewMatrix);
+            var bl = Vector2.Transform(new Vector2(0, _screenSize.Y), inverseViewMatrix);
+            var br = Vector2.Transform(_screenSize, inverseViewMatrix);
+            var min = new Vector2(
+                MathHelper.Min(tl.X, MathHelper.Min(tr.X, MathHelper.Min(bl.X, br.X))),
+                MathHelper.Min(tl.Y, MathHelper.Min(tr.Y, MathHelper.Min(bl.Y, br.Y))));
+            var max = new Vector2(
+                MathHelper.Max(tl.X, MathHelper.Max(tr.X, MathHelper.Max(bl.X, br.X))),
+                MathHelper.Max(tl.Y, MathHelper.Max(tr.Y, MathHelper.Max(bl.Y, br.Y))));
+            return new Rectangle((int)min.X, (int)min.Y, (int)(max.X - min.X), (int)(max.Y - min.Y));
         }
     }
 }
